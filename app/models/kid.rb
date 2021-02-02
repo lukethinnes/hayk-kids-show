60b8f18d22a3f68f79278904bb6e956e83f99681
Luke Thinnes <lukethinnes@yahoo.com>
2021-02-02 14:33:04 -0700

    Done.

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,2 +1,8 @@
 class Kid < ApplicationRecord
+    validates :age, presence: true, numericality: {
+        only_integer: true,
+        greater_than: 6,
+        less_than: 15 
+    }
+    validates :username, length: {minimum: 7, maximum: 25}, presence: true, uniqueness: true, 
 end
